920c054e9fb8a7086860466825b9e1b9ca4153c2
Abel Nieva <you@example.com>
2017-12-14 23:39:45 -0300

    added support ubuntu 1604

--- recipes/monitoring.rb
+++ recipes/monitoring.rb
@@ -8,7 +8,7 @@
 #
 
 package_name = "mongodb-mms-monitoring-agent"
-package_file = "#{package_name}_#{node['mongodb-agents']['monitoring']['version']}_#{node['kernel']['machine'] == 'x86_64' ? 'amd64' : 'i386'}.deb"
+package_file = "#{package_name}_#{node['mongodb-agents']['monitoring']['version']}_#{node['kernel']['machine'] == 'x86_64' ? 'amd64' : 'i386'}.#{node['platform_version'] == '16.04' ? 'ubuntu1604' : ''}.deb"
 download_url = "#{node['mongodb-agents']['agent_url']}/monitoring/#{package_file}"
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{package_file}" do
@@ -37,7 +37,15 @@ template "/etc/mongodb-mms/monitoring-agent.config" do
 end
 
 service "mongodb-mms-monitoring-agent" do
-  provider Chef::Provider::Service::Upstart
+
+  case node['platform']
+     when 'ubuntu'
+         if node['platform_version'] == '16.04'
+           provider Chef::Provider::Service::Systemd
+         end
+     else
+       provider Chef::Provider::Service::Upstart
+     end
   supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
 end
